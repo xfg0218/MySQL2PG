12237dda9fa92a004cd9ed440b216ec03a5147a9
xiaoxu <xufugui88@163.com>
2026-01-22 15:54:23 +0800

    add user test case

---
+++ scripts/mysql/create_user.sql
@@ -0,0 +1,14 @@
+-- 创建test1用户，密码为Abcd123，允许所有IP连接
+CREATE USER 'test1'@'%' IDENTIFIED BY 'Abcd123';
+
+-- 创建test2用户，密码为Abcd123，允许所有IP连接
+CREATE USER 'test2'@'%' IDENTIFIED BY 'Abcd123';
+
+-- 赋予test1用户所有数据库所有表的权限
+GRANT ALL PRIVILEGES ON *.* TO 'test1'@'%';
+
+-- 赋予test2用户所有数据库所有表的权限
+GRANT ALL PRIVILEGES ON *.* TO 'test2'@'%';
+
+-- 刷新权限
+FLUSH PRIVILEGES;
