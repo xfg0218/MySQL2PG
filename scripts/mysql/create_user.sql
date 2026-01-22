016974b80c77350594e4707d453459890aba3c34
xiaoxu <xufugui88@163.com>
2026-01-22 19:48:13 +0800

    update create user case

--- scripts/mysql/create_user.sql
+++ scripts/mysql/create_user.sql
@@ -1,7 +1,9 @@
 -- 创建test1用户，密码为Abcd123，允许所有IP连接
+DROP USER IF EXISTS 'test1'@'%';
 CREATE USER 'test1'@'%' IDENTIFIED BY 'Abcd123';
 
 -- 创建test2用户，密码为Abcd123，允许所有IP连接
+DROP USER IF EXISTS 'test2'@'%';
 CREATE USER 'test2'@'%' IDENTIFIED BY 'Abcd123';
 
 -- 赋予test1用户所有数据库所有表的权限
